deda96f165d30c1868fd3d8d4496e7306dac69f6
AmanzhanMuratov <60780605+AmanzhanMuratov@users.noreply.github.com>
2020-04-04 21:01:08 +0700

    Update c.py

--- c.py
+++ c.py
@@ -8,14 +8,14 @@ player_x = 306
 player_y = 550
 
 enemyImage = pygame.image.load("enemy.png")
-enemy_x = random.randint(0,736)
+enemy_x = random.randint(100,512)
 enemy_y = random.randint(20,50)
 enemy_dx = 5
 enemy_dy = 50
 
 bulletImage = pygame.image.load("bullet.png")
-bullet_x = 306
-bullet_y = 586
+bullet_x = 314
+bullet_y = 594
 
 backgroundImage = pygame.image.load("background.jpg")
 
@@ -27,6 +27,13 @@ def enemy(x,y):
 
 def bullet(x,y):
     screen.blit(bulletImage,(x,y))
+score = 0
+
+def drawScore():
+    global score
+    font = pygame.font.SysFont('smaller.fon', 30)
+    text = font.render(f'Score:{score}', 1, (0, 0, 0))
+    screen.blit(text, (530, 40))
 
 done = False
 while not done:
@@ -42,17 +49,19 @@ while not done:
             bullet_x = bullet_x+5
         if event.key == pygame.K_SPACE:
             if bullet_y < 0:
-                bullet_y += 500
+                bullet_y += 600
             bullet_y-=5
     dist_x = bullet_x - enemy_x
     dist_y = bullet_y - enemy_y
     
-    if -16 <= dist_x <= 48 and -16 <= dist_y <= 48 :
+    if -24 <= dist_x <= 64 and -24 <= dist_y <= 64 :
         bullet_x, bullet_y = 306, 586
-        enemy_x, enemy_y = random.randint(0, 736), random.randint(20, 50)
+        enemy_x, enemy_y = random.randint(100,512), random.randint(20, 50)
+        score+=1
 
     screen.fill((0,0,0))    
     screen.blit(backgroundImage,(0,0))
+    drawScore()
     player(player_x,player_y)
     bullet(bullet_x,bullet_y)
     enemy_x+=enemy_dx
